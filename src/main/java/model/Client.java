ff8b5eb0a6b3497ef38d61670d8377014f23e220
Lukasz <lukasztbg@gmail.com>
2020-01-13 18:24:06 +0100

    create model

---
+++ src/main/java/model/Client.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Client {
+    int id;
+    String firstName;
+    String lastName;
+    String address;
+    Order order;
+}
+
+
+//        orders			Order       	/jeden do wielu
---
+++ src/main/java/model/Restaurant.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Restaurant {
+    int id;
+    String name;
+    Food food;
+    Order order;
+}
+
+
+//        food			Food		/jeden do wielu
+//        orders			Order		/jeden do wielu
